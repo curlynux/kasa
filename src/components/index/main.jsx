528daf56e23ad94bcc09e44272e991b77db6605c
curlynux <curlynux@gmail.com>
2023-09-30 12:51:00 +0000

    randomize article render

--- src/components/index/main.jsx
+++ src/components/index/main.jsx
@@ -2,23 +2,43 @@ import "../../assets/css/index/main.css";
 
 function Main({ data }) {
 	console.log(data);
+	const shuffledData = [...data];
+	for (let i = shuffledData.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[shuffledData[i], shuffledData[j]] = [shuffledData[j], shuffledData[i]];
+	}
+
+	const firstSectionData = shuffledData.slice(
+		0,
+		Math.ceil(shuffledData.length / 2)
+	);
+	const secondSectionData = shuffledData.slice(
+		Math.ceil(shuffledData.length / 2)
+	);
+
 	return (
 		<main>
 			<section>
-				{data.map((element, index) => {
-					return (
-						<article>
-							<img src={element.cover} alt="location" />
-							<span></span>
-						</article>
-					);
-				})}
+				{firstSectionData &&
+					firstSectionData.map((element, index) => {
+						return (
+							<article>
+								<img src={element.cover} alt="location" />
+								<span>{element.title}</span>
+							</article>
+						);
+					})}
 			</section>
 			<section>
-				<article id="">
-					<img src="" alt="location" />
-					<span></span>
-				</article>
+				{secondSectionData &&
+					secondSectionData.map((element, index) => {
+						return (
+							<article>
+								<img src={element.cover} alt="location" />
+								<span>{element.title}</span>
+							</article>
+						);
+					})}
 			</section>
 		</main>
 	);
